f85c55326e2838010f21f385731ef2d0e3c842c0
Devtech Karhagomba <devtechkarhagomba@gmail.com>
2025-09-19 16:25:18 +0000

    I still get the same error

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,3 +1,4 @@
+
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApp, getApps } from "firebase/app";
 import { getAuth } from "firebase/auth";
@@ -7,12 +8,11 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  "projectId": "studio-5850099320-b46d2",
-  "appId": "1:539253016026:web:9d377e7e3275b8e2e12ce3",
-  "apiKey": "AIzaSyDJ0I0Z3biGynPucankJ1lW-oLGbfvcCpw",
-  "authDomain": "studio-5850099320-b46d2.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "539253016026"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
 };
 
 
